becfa7d5a1a4d3bc07902c71d494177e73365ae1
amar-besic <amar.besic.2002@gmail.com>
2025-01-07 14:11:55 +0100

    1.7.2025.

---
+++ ftp.fit.ba/Upload/cs-winforms-exam-template-2023-24-main/FIT.WinForms/frmPocetna.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FIT.WinForms
+{
+    partial class frmPocetna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblKonekcijaInfo = new Label();
+            btnIzvjestaj = new Button();
+            btnDržave = new Button();
+            btnPretraga = new Button();
+            SuspendLayout();
+            // 
+            // lblKonekcijaInfo
+            // 
+            lblKonekcijaInfo.AutoSize = true;
+            lblKonekcijaInfo.Font = new Font("Segoe UI", 15F);
+            lblKonekcijaInfo.Location = new Point(114, 96);
+            lblKonekcijaInfo.Name = "lblKonekcijaInfo";
+            lblKonekcijaInfo.Size = new Size(0, 35);
+            lblKonekcijaInfo.TabIndex = 0;
+            // 
+            // btnIzvjestaj
+            // 
+            btnIzvjestaj.Location = new Point(158, 133);
+            btnIzvjestaj.Margin = new Padding(3, 4, 3, 4);
+            btnIzvjestaj.Name = "btnIzvjestaj";
+            btnIzvjestaj.Size = new Size(207, 31);
+            btnIzvjestaj.TabIndex = 1;
+            btnIzvjestaj.Text = "Izvještaj";
+            btnIzvjestaj.UseVisualStyleBackColor = true;
+            btnIzvjestaj.Click += btnIzvjestaj_Click;
+            // 
+            // btnDržave
+            // 
+            btnDržave.Location = new Point(114, 171);
+            btnDržave.Name = "btnDržave";
+            btnDržave.Size = new Size(151, 62);
+            btnDržave.TabIndex = 2;
+            btnDržave.Text = "Države i gradovi";
+            btnDržave.UseVisualStyleBackColor = true;
+            btnDržave.Click += btnDržave_Click;
+            // 
+            // btnPretraga
+            // 
+            btnPretraga.Location = new Point(271, 171);
+            btnPretraga.Name = "btnPretraga";
+            btnPretraga.Size = new Size(151, 62);
+            btnPretraga.TabIndex = 2;
+            btnPretraga.Text = "Pretraga";
+            btnPretraga.UseVisualStyleBackColor = true;
+            // 
+            // frmPocetna
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(514, 245);
+            Controls.Add(btnPretraga);
+            Controls.Add(btnDržave);
+            Controls.Add(btnIzvjestaj);
+            Controls.Add(lblKonekcijaInfo);
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmPocetna";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Template 2023/24";
+            Load += frmPocetna_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblKonekcijaInfo;
+        private Button btnIzvjestaj;
+        private Button btnDržave;
+        private Button btnPretraga;
+    }
+}
